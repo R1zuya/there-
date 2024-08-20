9c4dc1264c98d1449c9ab80d62c9662d971dddba
R1zuya <rizuya.kituno@gmail.com>
2024-08-20 13:08:52 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -1,16 +1,23 @@
-import os
-
 class Config(object):
     LOGGER = True
-    OWNER_ID = os.getenv("OWNER_ID")
-    sudo_users = os.getenv("SUDO_USERS", "").split(",")
-    GROUP_ID = int(os.getenv("GROUP_ID", 0))
-    TOKEN = os.getenv("BOT_TOKEN")
-    mongo_url = os.getenv("MONGO_URL")
-    PHOTO_URL = os.getenv("PHOTO_URL", "").split(",")
-    SUPPORT_CHAT = os.getenv("SUPPORT_CHAT")
-    UPDATE_CHAT = os.getenv("UPDATE_CHAT")
-    BOT_USERNAME = os.getenv("BOT_USERNAME")
-    CHARA_CHANNEL_ID = int(os.getenv("CHARA_CHANNEL_ID", 0))
-    api_id = int(os.getenv("API_ID", 0))
-    api_hash = os.getenv("API_HASH")
+
+    # Get this value from my.telegram.org/apps
+     OWNER_ID="7368341648"
+     SUDO_USERS="7368341648, 1102584877, 6653725844, 2107880985, 6149361523, 7013604559"
+     GROUP_ID="-1001593116522"
+     BOT_TOKEN="6913009476:AAE1fdTf_TvbDCBNWiAh1XyHhchpeftUez4"
+     MONGO_URL="mongodb+srv://Douma:Douma@douma.yjsryni.mongodb.net"
+     PHOTO_URL="url1,url2"
+     SUPPORT_CHAT="Anime_chat_enigmatic"
+     UPDATE_CHAT="anime_chat_enigmatic"
+     BOT_USERNAME="@guess_waifu_robot" 
+     CHARA_CHANNEL_ID="-1002091239384"
+     API_ID="6252332"
+     API_HASH="2bf5e96a75ca073fd4f37ca9562971d3"
+    
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
